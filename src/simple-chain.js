15754ed02f96282cdebd5e953510312e45c466af
mayerror <mayerror@gmail.com>
2021-03-06 03:08:03 +0300

    feat: implement simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    "use strict";
+    this.chain.push(this.chain.length);
+    return this;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    "use strict";
+    if (typeof value === 'object'){
+      if(value === null) {
+        this.chain.push('null');
+      } else {
+        this.chain.push('[object Object]');
+      }
+    } else if (typeof value === 'function') {
+      this.chain.push('function() {}');
+    } else {
+      this.chain.push(value);
+    }
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    "use strict";
+    if (this.chain.length < position || position < 1 || !Number.isInteger(position)) {
+      this.chain = [];
+      throw "Error";
+    } else {
+      this.chain.splice(position-1,1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    "use strict";
+    if (this.chain.length > 1) {
+      this.chain.reverse();
+    }
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    "use strict";
+    let str = '( ' + this.chain.join(' )~~( ') + ' )';
+    this.chain = [];
+    return str;
   }
 };
 
