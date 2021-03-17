ba8c06c76989e8c3aab2313e86fa3baaa2c1a5b2
mayerror <mayerror@gmail.com>
2021-03-17 16:52:07 +0300

    feat: implement empty array case for transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// module.exports = 
+function transform(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  if (!Array.isArray(arr)) {
+    throw "Error";
+  }
+  let resultArr = [],
+      i = 0;
+  arr.forEach((element, index) => {
+    if(element !== ('--discard-next' || '--discard-prev' || '--double-next' || '--double-prev') && index === 0) {
+      resultArr[i] = element;
+    }
+  };
+  return resultArr;
 };
+
+console.log(transform(['sdasd', 1, 3]));
