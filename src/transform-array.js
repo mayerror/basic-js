cc97fa85d502a6c8ae1113c5a99f494f3e38d9ad
mayerror <mayerror@gmail.com>
2021-03-19 16:57:42 +0300

    feat: add transform array function implementation

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-// module.exports = 
+module.exports = 
 function transform(arr) {
   if (arr.length === 0) {
     return [];
@@ -9,13 +9,35 @@ function transform(arr) {
     throw "Error";
   }
   let resultArr = [],
-      i = 0;
+      statusPrevDeleted = 0;
   arr.forEach((element, index) => {
-    if(element !== ('--discard-next' || '--discard-prev' || '--double-next' || '--double-prev') && index === 0) {
-      resultArr[i] = element;
+    if(element === '--discard-prev') {
+      if (resultArr.length > 0 && statusPrevDeleted === 0) {
+        resultArr.pop();
+      }
+      statusPrevDeleted = 0;
+    } else if(element === '--double-prev') {
+      if (resultArr.length > 0 && statusPrevDeleted === 0) {
+        resultArr.push(arr[index-1]);
+      }
+      statusPrevDeleted = 0;
+    } else if (element === '--double-next') {
+      if (index !== (arr.length-1)) {
+        resultArr.push(arr[index+1]);
+      }
+      statusPrevDeleted = 0;
+    } else if (element === '--discard-next') {
+      if (index !==(arr.length-1)) {
+        statusPrevDeleted++;
+      }
+    } else {
+      if (statusPrevDeleted === 0 || statusPrevDeleted === 2) {
+        resultArr.push(element);
+        statusPrevDeleted = 0;
+      } else statusPrevDeleted++;
     }
-  };
+  });
   return resultArr;
 };
 
-console.log(transform(['sdasd', 1, 3]));
+// console.log(transform(['sdasd', 1, 3, '--double-prev']));
